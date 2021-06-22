ed196204ac98b8124c1b4d97ac9e4ac3c9851da5
DePrincio <princetlive@yahoo.com>
2021-06-22 15:41:46 -0400

    display weather details

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLazyQuery } from '@apollo/client';
 import { GET_WEATHER_QUERY } from '../graphql/Queries';
 
 export default function Home() {
-	const [getWeather, { loading, error }] = useLazyQuery(GET_WEATHER_QUERY, {
-		variables: {
-			name: 'Vancouver',
-		},
-	});
+	const [citySearched, setCitySearched] = useState('');
+
+	const [getWeather, { loading, error, data }] = useLazyQuery(
+		GET_WEATHER_QUERY,
+		{
+			variables: {
+				name: citySearched,
+			},
+		}
+	);
+
+	useEffect(() => {
+		if (error) return <h1>Error found</h1>;
+		if (data) {
+			console.log(data);
+		}
+	}, [data]);
 	return (
 		<div className='home'>
 			<h1>Search for Weather</h1>
-			<input type='text' placeholder='City Name....' />
+			<input
+				type='text'
+				placeholder='City Name....'
+				onChange={(event) => {
+					setCitySearched(event.target.value);
+				}}
+			/>
 			<button onClick={() => getWeather()}>Search</button>
+
+			<div>
+				<h1>City Name: {data.getCityByName.name}</h1>
+				<h1>Temperature: {data.getCityByName.weather.temperature.actual}</h1>
+				<h1>Description: {data.getCityByName.weather.summary.description}</h1>
+				<h1>Wind: {data.getCityByName.weather.wind.speed}</h1>
+			</div>
 		</div>
 	);
 }
