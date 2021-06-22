71330814eb612e1e6764a7718f51a12861462f2d
DePrincio <princetlive@yahoo.com>
2021-06-22 13:54:11 -0400

    set up the queries

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react'
-import Home from './components/Home'
+import React from 'react';
+import Home from './components/Home';
+import {
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider,
+	HttpLink,
+} from '@apollo/client';
 
 export default function App() {
-  return (
-    <div>
-      <Home />
-    </div>
-  )
+	const client = new ApolloClient({
+		cache: new InMemoryCache(),
+		uri: 'https://graphql-weather-api.herokuapp.com/',
+	});
+	return (
+		<ApolloProvider client={client}>
+			<Home />
+		</ApolloProvider>
+	);
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { useLazyQuery } from '@apollo/client';
+import { GET_WEATHER_QUERY } from '../graphql/Queries';
 
 export default function Home() {
-  return (
-    <div className='home'>
-      <h1>Search for Weather</h1>
-      <input type="text" placeholder='City Name....' />
-      <button>Search</button>
-      
-    </div>
-  )
+	const [getWeather, { loading, error }] = useLazyQuery(GET_WEATHER_QUERY, {
+		variables: {
+			name: 'Vancouver',
+		},
+	});
+	return (
+		<div className='home'>
+			<h1>Search for Weather</h1>
+			<input type='text' placeholder='City Name....' />
+			<button onClick={() => getWeather()}>Search</button>
+		</div>
+	);
 }
