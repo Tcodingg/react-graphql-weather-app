a33048ec1500d248315964b1e759caaffb2964f5
DePrincio <princetlive@yahoo.com>
2021-06-22 12:58:16 -0400

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='home'>
+      <h1>Search for Weather</h1>
+      <input type="text" placeholder='City Name....' />
+      <button>Search</button>
+      
+    </div>
+  )
+}
